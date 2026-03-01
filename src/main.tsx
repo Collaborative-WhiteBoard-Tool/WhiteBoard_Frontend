7df6f6cfe9c9bb2b412375f7cf8a56a4e6338e5f
Minh200463 <kuidndjj@gmail.com>
2026-03-01 20:56:28 +0700

    update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,20 @@
 // import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App'
-import { BrowserRouter } from 'react-router-dom'
-import { Toaster } from './components/ui/sonner'
-<script src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1" type="module"></script>
-// import './styles/'
-
-createRoot(document.getElementById('root')!).render(
-
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { Toaster } from "./components/ui/sonner";
+<script
+  src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1"
+  type="module"
+></script>;
+if (import.meta.env?.PROD || process.env.NODE_ENV === "production") {
+  console.log = () => {};
+  console.warn = () => {};
+}
+createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <App />
     <Toaster />
-  </BrowserRouter>
-
-)
+  </BrowserRouter>,
+);
